f7d46362390906fd0b918da6fb915097f7bd8afd
pastuxso <pastuxso@5c60d122-7a0f-0410-b01c-879a277d15ac>
2006-04-20 16:41:10 +0000

    Depurando repositorio

--- trunk/beta/Common/common/gui/formas/ChangeExternalValueListener.java
+++
@@ -1,7 +0,0 @@
-package common.gui.formas;
-
-import java.util.EventListener;
-
-public interface ChangeExternalValueListener extends EventListener {
-	public void changeExternalValue(ChangeExternalValueEvent e);
-}
