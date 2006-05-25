1fe6949459b3ead989c0c2a214e7eb86900f9dda
pastuxso <pastuxso@5c60d122-7a0f-0410-b01c-879a277d15ac>
2006-05-25 00:38:49 +0000

    Depurando codigo fuente

--- trunk/beta/Common/common/printer/TextGenerator.java
+++ trunk/beta/Common/common/printer/TextGenerator.java
@@ -1,6 +1,7 @@
 package common.printer;
 import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class TextGenerator {
 	
@@ -13,12 +14,10 @@ public class TextGenerator {
 	public void addTextArea(String text,int row, int col, int width, int height) {
 		
 		text = text.replaceAll("\n", "");
-		int max = width*height;
 		StringBuffer buf = new StringBuffer(text.trim());
 		int lastspace = -1;
 		int linestart = 0;
 		int i = 0;
-		
 		
 		while (i < buf.length()) {
 			if (buf.charAt(i) == ' ') {
@@ -28,15 +27,14 @@ public class TextGenerator {
 				lastspace = -1;
 				linestart = i + 1;
 			}
-			if (i > linestart + width -1) {
+			if (i > linestart + width) {
+				//if (i > linestart + width -1) {
 				if (lastspace != -1) {
-					String add = "\n";
-					for (int k=0; k < (i-lastspace)-1;k++) {
-						add = " ".concat(add); 
-					}
-					buf.insert(lastspace+1,add);
-					linestart = lastspace + 1;
+					buf.delete(lastspace,lastspace+1);
+					buf.insert(lastspace,"\n");
+					linestart = lastspace;
 					lastspace = -1;
+					
 				} else {
 					buf.insert(i, '\n');
 					linestart = i + 1;
@@ -44,21 +42,10 @@ public class TextGenerator {
 			}
 			i++;
 		}
-		int end = max > buf.length() ? buf.length() : max;
-		String textArea = buf.toString().substring(0,end);
-		int j=0;
-		String acum = "";
-		while(j  < textArea.length() ) {
-			if (textArea.charAt(j)!='\n') {
-				acum += textArea.charAt(j); 
-			}
-			else {
-				addString(acum.trim(),row++,col,null);
-				acum ="";
-			}
-			j++;
+		StringTokenizer st = new StringTokenizer(buf.toString(),"\n");
+		for (int j=0; j < height ; j++) {
+			addString(st.nextToken(),row++,col,null);
 		}
-		addString(acum.trim(),row++,col,null);
 	}
 	
 	public void addString(String str, int row, int col,Integer width) {
