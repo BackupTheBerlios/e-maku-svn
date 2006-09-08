28f486a2d9208b76a2a983b7e142cc1a9285966d
xtingray <xtingray@5c60d122-7a0f-0410-b01c-879a277d15ac>
2006-09-08 16:22:08 +0000

    Actualizando jar de impresion

--- trunk/beta/build.sh
+++ trunk/beta/build.sh
@@ -148,7 +148,11 @@ install_server() {
              cp -rf $ROOT/reports $EMAKU_HOME/
 
              mkdir -p $EMAKU_HOME/conf
-             cp -f $ROOT/conf.unix/server.conf $EMAKU_HOME/conf
+
+             if [ ! -e $EMAKU_HOME/conf/server.conf ] ; then
+               cp -f $ROOT/conf.unix/server.conf $EMAKU_HOME/conf
+             fi
+             
              cp -f $ROOT/conf.unix/wrapper.conf $EMAKU_HOME/conf
 
              ARCH=32
